878c85678e2cc780e34dadc2f9f37d26600f2d07
David E. Kling <dkling@broadinstitute.org>
2015-06-23 15:14:27 -0400

    Changed documentation

--- src/java/picard/analysis/directed/CalculateHsMetrics.java
+++ src/java/picard/analysis/directed/CalculateHsMetrics.java
@@ -66,8 +66,7 @@ import java.util.TreeSet;
                 "     -R=ReferenceSequence.fasta \\<br />" +
                 "     -BAIT_INTERVALS=Baitintervallist.vcf \\<br />" +
                 "     -TARGET_INTERVALS=Targetintervallist.vcf" +
-                "</pre> "
-        ,
+                "</pre> ",
         usageShort = "Calculates a set of Hybrid Selection-specific metrics from an aligned SAM or BAM file ",
         programGroup = Metrics.class
 )
